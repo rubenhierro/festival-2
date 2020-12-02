cc2c4b92b827bc9f476ab6b735f550f7680fdde0
Rubèn Hierro <rubenhierro@gmail.com>
2020-12-02 12:24:21 +0100

    Initial config

---
+++ stylelint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "extends": "stylelint-config-standard",
+  "plugins": [
+    "stylelint-scss"
+  ],
+  "rules": {
+    "indentation": 2,
+    "no-descending-specificity": null,
+    "no-eol-whitespace": null,
+    "declaration-empty-line-before": null,
+    "value-keyword-case": null,
+    "at-rule-no-unknown": [
+      true,
+      {
+        "ignoreAtRules": [
+          "tailwind",
+          "apply",
+          "responsive",
+          "variants",
+          "screen",
+          "use"
+        ]
+      }
+    ]
+  }
+}
